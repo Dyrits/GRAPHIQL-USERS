8df7bfa30a045ca19cfc9d7adb59336331469226
Dylan J. Gerrits <dyrits@outlook.com>
2023-05-23 11:08:23 +0200

    GraphQL Schema [++] CompanyType

--- schema/schema.js
+++ schema/schema.js
@@ -1,17 +1,41 @@
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLID, GraphQLString, GraphQLInt
+  GraphQLID, GraphQLString, GraphQLInt, GraphQLList
 } = require("graphql");
-const axios = require("axios");
+
+const json$server = require("axios").create({
+  baseURL: "http://localhost:3000"
+})
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(source, args) {
+        return json$server.get(`/companies/${source.id}/users`).then(({ data }) => data);
+      }
+    }
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
-  }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(source, args) {
+        return json$server.get(`/companies/${source.company}`).then(({ data }) => data);
+      }
+    }
+  })
 });
 
 const RootQueryType = new GraphQLObjectType({
@@ -21,7 +45,14 @@ const RootQueryType = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLID } },
       resolve(source, args) {
-        return axios.get(`http://localhost:3000/users/${args.id}`).then(({ data }) => data);
+        return json$server.get(`/users/${args.id}`).then(({ data }) => data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLID } },
+      resolve(source, args) {
+        return json$server.get(`/companies/${args.id}`).then(({ data }) => data);
       }
     }
   }
